40e3198e65425e73474dd0ef3747fdc805e4d244
lianghua1987 <hualiang.987@gmail.com>
2022-08-14 19:30:34 +0800

    connect to firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-    const [value, setValue] = useState('');
+    // const [value, setValue] = useState('');
+    const [tmp, setTmp] = useState('');
+    const [history, setHistory] = useState([]);
+    useEffect(() => {
+        get().catch(e => console.error(e));
+
+    }, []);
 
     const onChange = e => {
-        setValue(e.target.value);
+        setTmp(e.target.value);
     }
+    const submit = () => {
+        fetch('https://win-mac-connector-default-rtdb.asia-southeast1.firebasedatabase.app/values.json',
+            {
+                method: 'POST',
+                body: JSON.stringify({value: tmp}),
+                header: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(() => {
+        })
+    }
+
+    const get = async () => {
+        const resp = await fetch('https://win-mac-connector-default-rtdb.asia-southeast1.firebasedatabase.app/values.json?orderBy="$key"&limitToLast=1');
+        const data = await resp.json();
+        const histories = [];
+        for (const v in data) {
+            histories.push(data[v].value);
+        }
+        setHistory(histories);
+        console.log(histories);
+    }
+
 
     return (
         <div className="App">
             <header className="App-header">
                 <label>Windows -> Mac Message Connector</label><br/>
+                <textarea rows="5" onChange={onChange}></textarea>
                 <div>
-                    <textarea rows="5" onChange={onChange}></textarea>
-                    <p className="info">
-                        {value}
-                    </p>
+                    <button onClick={submit}>submit</button>
+                    <button onClick={get}>fetch</button>
+
+                    <ul>
+                        {history.map(v => {
+                            return <li>{v}</li>
+                        })}
+                    </ul>
                 </div>
 
             </header>
